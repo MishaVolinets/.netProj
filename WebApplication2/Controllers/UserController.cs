7ec9bc5322c8fb09b9a941f3e1e2f5ce4c4ad5ed
MishaVolinets <VolinetsMisha@gmail.com>
2016-09-26 11:44:06 +0300

    First Commit

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Information()
+        {
+            User us1 = new User("Misha",14);
+            return View(us1);
+        }
+    }
+}
